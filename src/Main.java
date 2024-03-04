0f01e67a121235e1fc08825cc0dbdbd05ea12e36
Mikhail Trifonov <trifonov2812@gmail.com>
2024-03-04 21:40:52 +0300

    Correct output but TL

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,51 @@
 // Ideas for rotating, balancing and insertion were taken from Lecture 07 (slide № 45), Tutorial 07 (slide № 16), and Wiki
 // Idea about finding new height using maximum was taken from the lab by Alaa Aldin
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
 public class Main {
+    private static ArrayList<ArrayList<Node<Integer>>> res = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         AVLTree<Integer> avlTree = new AVLTree<>();
+        int mx = -10000;
+        int[] nums = new int[n];
         for (int i = 0; i < n; i++) {
             int value = scanner.nextInt();
-            avlTree.insert(value, i + 1);
+            mx = Math.max(mx, value);
+            nums[i] = value;
+            avlTree.insert(value);
         }
+        for (int i = 0; i < mx + 1; i++) {
+            res.add(null);
+        }
+
         System.out.println(n);
+
         printResult(avlTree.getRoot());
-        System.out.println(avlTree.getRoot().getIndex());
+        for (int i = 0; i < n; i++) {
+            ArrayList<Node<Integer>> nodes = res.get(nums[i]);
+            StringBuilder line = new StringBuilder();
+            line.append(nums[i]).append(" ");
+            int ind1 = nodes.get(0) == null ? -1 : findIndexByElement(nums, nodes.get(0).getValue(), n) + 1;
+            int ind2 = nodes.get(1) == null ? -1 : findIndexByElement(nums, nodes.get(1).getValue(), n) + 1;
+            line.append(ind1).append(" ");
+            line.append(ind2);
+            System.out.println(line);
+        }
+
+        System.out.println(findIndexByElement(nums, avlTree.getRoot().getValue(), n) + 1);
+    }
+
+    private static int findIndexByElement(int[] nums, int a, int n) {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == a) return i;
+        }
+        return -1;
     }
 
     private static void printResult(Node<Integer> root) {
@@ -25,32 +55,27 @@ public class Main {
 
     private static void printOneNode(Node<Integer> node) {
         if (node != null) {
+            ArrayList<Node<Integer>> nodes = new ArrayList<>();
+            nodes.add(node.getLeft());
+            nodes.add(node.getRight());
+            res.set(node.getValue(), nodes);
             printOneNode(node.getLeft());
-            System.out.println(node.getValue() + " " + (node.getLeft() == null ? -1 : node.getLeft().getIndex()) + " " +
-                    (node.getRight() == null ? -1 : node.getRight().getIndex()));
             printOneNode(node.getRight());
         }
     }
 }
-
 
 class Node<T> {
     private T value;
     private int height;
     private Node<T> left;
     private Node<T> right;
-    private int index;
 
-    public Node(T value, int index) {
+    public Node(T value) {
         this.value = value;
         this.height = 1;
         this.left = null;
         this.right = null;
-        this.index = index;
-    }
-
-    public int getIndex() {
-        return index;
     }
 
     public T getValue() {
@@ -117,37 +142,33 @@ class AVLTree<T extends Comparable<T>> {
         return y;
     }
 
-    public void insert(T value, int index) {
-        root = insert(root, value, index);
+    public void insert(T value) {
+        root = insert(root, value);
     }
 
-    private Node<T> insert(Node<T> node, T value, int index) {
+    private Node<T> insert(Node<T> node, T value) {
         if (node == null)
-            return new Node<>(value, index);
+            return new Node<>(value);
         if (value.compareTo(node.getValue()) < 0)
-            node.setLeft(insert(node.getLeft(), value, index));
+            node.setLeft(insert(node.getLeft(), value));
         else if (value.compareTo(node.getValue()) > 0)
-            node.setRight(insert(node.getRight(), value, index));
+            node.setRight(insert(node.getRight(), value));
 
         node.setHeight(Math.max(notNullHeight(node.getLeft()), notNullHeight(node.getRight())) + 1);
 
         int balance = notNullHeight(node.getLeft()) - notNullHeight(node.getRight());
 
-        // Left Left Case
         if (balance > 1 && value.compareTo(node.getLeft().getValue()) < 0)
             return rotateRight(node);
 
-        // Right Right Case
         if (balance < -1 && value.compareTo(node.getRight().getValue()) > 0)
             return rotateLeft(node);
 
-        // Left Right Case
         if (balance > 1 && value.compareTo(node.getLeft().getValue()) > 0) {
             node.setLeft(rotateLeft(node.getLeft()));
             return rotateRight(node);
         }
 
-        // Right Left Case
         if (balance < -1 && value.compareTo(node.getRight().getValue()) < 0) {
             node.setRight(rotateRight(node.getRight()));
             return rotateLeft(node);
